9b6a88873e5fcd20c212900233c3974b04b8cdcc
uxunity <motioncensor22443@gmail.com>
2025-02-11 18:45:32 -0600

    Oxygen Site In Development Still

---
+++ script.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            const navbarHeight = document.querySelector(".navbar").offsetHeight;
+
+            window.scrollTo({
+                top: targetElement.offsetTop - navbarHeight, // Adjust for navbar height
+                behavior: "smooth",
+            });
+        }
+    });
+});
